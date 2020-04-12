ccab2e00ea8ea7fe2344fa8225820747de5a70ca
itochterman <irt221@nyu.edu>
2020-04-12 19:54:02 -0400

    working email service

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const creds = require("./config/config.js");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080",
+  origin: "http://localhost:8081",
   origin: "http://localhost:3000",
 };
 
@@ -20,69 +20,64 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
+// db.sequelize.sync();
+
+// simple route
 app.get("/", (req, res) => {
-  res.json({ message: "hi poems!." });
+  res.json({ message: "hi der." });
+});
+var transport = {
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("Uh oh", error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
 });
 
-// var transport = {
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: creds.USER,
-//     pass: creds.PASS,
-//   },
-// };
+app.post("/send", (req, res, next) => {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  var body = req.body.message;
+  var content = `name: ${
+    (firstName, lastName)
+  } \n email: ${email} \n message: ${body} `;
+  console.log("REQUEST: ", req.body);
+  var mail = {
+    from: firstName,
+    to: "itochterman@gmail.com",
+    subject: "New Message from Contact Form",
+    text: content,
+  };
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: "fail",
+      });
+    } else {
+      res.json({
+        msg: "success",
+      });
+    }
+  });
+});
 
-// var transporter = nodemailer.createTransport(transport);
+// set port, listen for requests
 
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log("Uh oh", error);
-//   } else {
-//     console.log("Server is ready to take messages");
-//   }
-// });
-
-// //move to separate file
-// app.post("/send", (req, res, next) => {
-//   var firstName = req.body.firstName;
-//   var lastName = req.body.lastName;
-//   var email = req.body.email;
-//   var body = req.body.message;
-//   var content = `name: ${
-//     (firstName, lastName)
-//   } \n email: ${email} \n message: ${body} `;
-//   console.log("REQUEST: ", req.body);
-//   var mail = {
-//     from: firstName,
-//     to: "itochterman@gmail.com",
-//     subject: "New Message from Contact Form",
-//     text: content,
-//   };
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         msg: "fail",
-//       });
-//     } else {
-//       res.json({
-//         msg: "success",
-//       });
-//     }
-//   });
-// });
-
-var poems = express.Router();
-var email = express.Router();
-
-require("./routes/poem.routes")(poems);
-require("./routes/email.routes")(email);
-
-app.use("/", poems);
-app.use("/", email);
-
-// app.use(require("./routes/email"));
+require("./routes/poem.routes")(app);
+console.log(app.response);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
