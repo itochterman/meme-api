156b28d1da9af25aba2cc2c34783ec80fa89996b
itochterman <irt221@nyu.edu>
2020-04-12 19:33:13 -0400

    add email service

--- controllers/poem.controller.js
+++ controllers/poem.controller.js
@@ -2,30 +2,26 @@ const db = require("../models");
 const Poem = db.poems;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
 exports.create = (req, res) => {
   console.log(req);
-  // Validate request
   if (!req.body.title) {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
   }
 
-  // Create a Tutorial
   const poem = {
     title: req.body.title,
     description: req.body.description,
-    published: req.body.published ? req.body.published : false
+    published: req.body.published ? req.body.published : false,
   };
 
-  // Save Tutorial in the database
   Poem.create(poem)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while creating the poem."
+        message: err.message || "Some error occurred while creating the poem.",
       });
     });
 };
@@ -35,28 +31,26 @@ exports.findAll = (req, res) => {
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
   Poem.findAll({ where: condition })
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving poems."
+        message: err.message || "Some error occurred while retrieving poems.",
       });
     });
 };
-
-// Find a single Tutorial with an id
 
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
   Poem.findByPk(id)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error retrieving poem with id=" + id
+        message: "Error retrieving poem with id=" + id,
       });
     });
 };
@@ -66,75 +60,72 @@ exports.update = (req, res) => {
   const id = req.params.id;
 
   Tutorial.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-    .then(num => {
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was updated successfully."
+          message: "Tutorial was updated successfully.",
         });
       } else {
         res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating Tutorial with id=" + id,
       });
     });
 };
 
-// Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
 
   Poem.destroy({
-    where: { id: id }
+    where: { id: id },
   })
-    .then(num => {
+    .then((num) => {
       if (num == 1) {
         res.send({
-          message: "Poem was deleted successfully!"
+          message: "Poem was deleted successfully!",
         });
       } else {
         res.send({
-          message: `Cannot delete Poem with id=${id}. Maybe Poem was not found!`
+          message: `Cannot delete Poem with id=${id}. Maybe Poem was not found!`,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Could not delete Poem with id=" + id
+        message: "Could not delete Poem with id=" + id,
       });
     });
 };
 
-// Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
   Poem.destroy({
     where: {},
-    truncate: false
+    truncate: false,
   })
-    .then(nums => {
+    .then((nums) => {
       res.send({ message: `${nums} Poems were deleted successfully!` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while removing all poems."
+        message: err.message || "Some error occurred while removing all poems.",
       });
     });
 };
-// Find all published Tutorials
 exports.findAllPublished = (req, res) => {
   Poem.findAll({ where: { published: true } })
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving poems."
+        message: err.message || "Some error occurred while retrieving poems.",
       });
     });
 };
---
+++ routes/email.routes.js
@@ -0,0 +1,52 @@
+module.exports = (app) => {
+  var nodemailer = require("nodemailer");
+  const creds = require("../config/config.js");
+
+  var transport = {
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: creds.USER,
+      pass: creds.PASS,
+    },
+  };
+
+  var transporter = nodemailer.createTransport(transport);
+
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log("Uh oh", error);
+    } else {
+      console.log("Server is ready to take messages");
+    }
+  });
+
+  app.post("/send", (req, res, next) => {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    var body = req.body.message;
+    var content = `name: ${
+      (firstName, lastName)
+    } \n email: ${email} \n message: ${body} `;
+    console.log("REQUEST: ", req.body);
+    var mail = {
+      from: firstName,
+      to: "itochterman@gmail.com",
+      subject: "New Message from Contact Form",
+      text: content,
+    };
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          msg: "fail",
+        });
+      } else {
+        res.json({
+          msg: "success",
+        });
+      }
+    });
+  });
+};
