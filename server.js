3d263c5bf03ac676399fad9df887a84cf4578eb6
itochterman <irt221@nyu.edu>
2020-04-12 19:58:23 -0400

    update server

--- server.js
+++ server.js
@@ -26,55 +26,56 @@ const db = require("./models");
 app.get("/", (req, res) => {
   res.json({ message: "hi der." });
 });
-var transport = {
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: creds.USER,
-    pass: creds.PASS,
-  },
-};
+// var transport = {
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: creds.USER,
+//     pass: creds.PASS,
+//   },
+// };
 
-var transporter = nodemailer.createTransport(transport);
+// var transporter = nodemailer.createTransport(transport);
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("Uh oh", error);
-  } else {
-    console.log("Server is ready to take messages");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("Uh oh", error);
+//   } else {
+//     console.log("Server is ready to take messages");
+//   }
+// });
 
-app.post("/send", (req, res, next) => {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  var body = req.body.message;
-  var content = `name: ${
-    (firstName, lastName)
-  } \n email: ${email} \n message: ${body} `;
-  console.log("REQUEST: ", req.body);
-  var mail = {
-    from: firstName,
-    to: "itochterman@gmail.com",
-    subject: "New Message from Contact Form",
-    text: content,
-  };
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: "fail",
-      });
-    } else {
-      res.json({
-        msg: "success",
-      });
-    }
-  });
-});
+// app.post("/send", (req, res, next) => {
+//   var firstName = req.body.firstName;
+//   var lastName = req.body.lastName;
+//   var email = req.body.email;
+//   var body = req.body.message;
+//   var content = `name: ${
+//     (firstName, lastName)
+//   } \n email: ${email} \n message: ${body} `;
+//   console.log("REQUEST: ", req.body);
+//   var mail = {
+//     from: firstName,
+//     to: "itochterman@gmail.com",
+//     subject: "New Message from Contact Form",
+//     text: content,
+//   };
+//   transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       res.json({
+//         msg: "fail",
+//       });
+//     } else {
+//       res.json({
+//         msg: "success",
+//       });
+//     }
+//   });
+// });
 
 // set port, listen for requests
+require("./routes/email.routes")(app);
 
 require("./routes/poem.routes")(app);
 console.log(app.response);
