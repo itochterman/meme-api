c146cfeb23cfa96bc9988e14c8f0d48a66bd9548
itochterman <irt221@nyu.edu>
2020-04-13 21:46:24 -0400

    switch server shit to product

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
+path.resolve("config", "config.js");
 let sequelize;
 // let dialect;
 
